fef275cf2704a5082c374ea4186cb6f99f7768b1
pinnouse <cupofpho@gmail.com>
2018-10-26 01:02:34 -0400

    Removed random 'what happened' log

--- bot.js
+++ bot.js
@@ -38,7 +38,6 @@ client.on('message', message => {
 });
 
 client.on('voiceStateUpdate', (oldMember, newMember) => {
-  console.log("what happened");
   if (client.voiceConnections.has(oldMember.guild.id)) {
     var voiceChannel = client.voiceConnections.get(oldMember.guild.id).channel;
     if (voiceChannel.members.filter(mem => { return mem.id != client.id; }).size <= 0) {
