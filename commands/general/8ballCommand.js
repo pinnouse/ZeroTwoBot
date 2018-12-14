434d3de31b2940356102fabf3cdb0f1ff0891c95
pinnouse <cupofpho@gmail.com>
2018-12-14 10:54:17 -0500

    8 ball command

---
+++ commands/general/8ballCommand.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const config = require('../../config.json');
+
+const utils = require('../../framework/utils');
+
+module.exports = {
+  name: '8ball',
+  category: 'general',
+  aliases: ['8ball', '8b'],
+  optArgs: [],
+  reqArgs: ['question'],
+  permissions: [],
+  unlimitedArgs: true,
+  description: (locale) => { return locale['general']['8ball']; },
+  executeCommand: async (args) => {
+    let locale = args.locale['general']['8ball'];
+
+    let response = locale.responses[Math.random() * locale.responses.length];
+    args.message.channel.send(
+      utils.getRichEmbed(
+        args.client,
+        0x000000,
+        locale.title,
+        response
+      )
+    );
+
+    return response;
+  }
+}
