8b6829313d8c37dd8e110afba7a1fbe311a53159
pinnouse <pinnouse@users.noreply.github.com>
2019-06-19 18:32:57 -0400

    Remove initial activity

--- bot.js
+++ bot.js
@@ -47,7 +47,6 @@ var chatModule = new ChatModule(client);
 let ready = false;
 client.on('ready', () => {
   console.log(`Bot is ready, logged in as: ${client.user.tag}\nwith prefix: ${prefix}`);
-  client.user.setActivity(`${client.guilds.size} channel(s)`, { type: "WATCHING" });
   ready = true;
 });
 
