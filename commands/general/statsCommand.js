ac4e1033842e3f6e77524bad1c4cfb03347d5041
pinnouse <pinnouse@users.noreply.github.com>
2019-07-28 23:50:14 -0400

    Fix stats command and update deps

--- commands/general/statsCommand.js
+++ commands/general/statsCommand.js
@@ -2,6 +2,10 @@
 
 const utils = require('../../framework/utils');
 
+const config = require('../../config.json');
+
+const prettyms = require('pretty-ms');
+
 module.exports = {
   name: 'stats',
   category: 'general',
@@ -12,12 +16,19 @@ module.exports = {
   unlimitedArgs: false,
   description: (locale) => { return locale['general']['stats']; },
   executeCommand: async (args) => {
+    let headers = args.locale['general']['stats']['headers'];
     args.message.channel.send(
       utils.getRichEmbed(
         args.client,
         0x303030,
         args.locale['general']['stats'].title
       )
+      .addField(headers.botHash, args.client.user.tag, true)
+      .addField(headers.voiceConnections, args.client.voiceConnections.size, true)
+      .addField(headers.guildsUsers, `${args.client.guilds.size} : ${(()=>{let count = 0; args.client.guilds.forEach(guild => count += guild.memberCount); return count;})()}`, true)
+      .addField(headers.uptime, prettyms(args.client.uptime), true)
+      .addField(headers.locale, config.defaultLang, true)
+      .addField(headers.prefix, config.prefix, true)
     );
 
     return true;
