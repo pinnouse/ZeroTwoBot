6dc3b79f58a4c3a35ca789f04d4699f726de2c59
pinnouse <cupofpho@gmail.com>
2019-10-05 21:14:25 -0400

    evaluate command whoops

---
+++ commands/development/evalCommand.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const config = require('../../config.json');
+
+const utils = require('../../framework/utils');
+
+module.exports = {
+  name: 'eval',
+  category: 'development',
+  aliases: ['eval'],
+  optArgs: [],
+  reqArgs: ['JS to evaluate'],
+  unlimitedArgs: true,
+  permissions: [],
+  showCommand: false,
+  description: (locale) => { return locale['development']['eval']; },
+  executeCommand: async (args) => {
+    // https://stackoverflow.com/questions/44072719/node-js-redirect-eval-output-to-string
+    let result = "";
+    let cons = {
+        log: (script) => {
+            result += script;
+        }
+    }
+    if (config.owners.includes(args.message.author.id)) {
+      let script = args.args.join(' ');
+      try {
+          let evaluate = eval(`((console) => { ${script} })`)(cons);
+          result += evaluate || "";
+      } catch(e) {
+          console.error(e);
+          await args.message.channel.send(
+              `message: \`${e.message}\`\n\nstack:\n\`\`\`${e.stack}\`\`\``
+          );
+      }
+      await args.message.channel.send(
+        `script: \`${script}\`\n\n result:\n${result ? `\`\`\`${result}\`\`\`` : '*```no output```*'}`
+      );
+      return true;
+    } else
+      await args.message.channel.send(
+        utils.getRichEmbed(args.client, 0xff0000, args.locale['development']['eval'].title, args.locale['development']['eval']['errors'].owner));
+    return false;
+  }
+}
